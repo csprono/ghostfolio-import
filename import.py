f27232736f501b135bcb0542e98b92f68fe9daf7
Adam Keating <adamkeating@xtra.co.nz>
2023-02-06 16:00:13 +1300

    fixed sending to api

--- import.py
+++ import.py
@@ -2,16 +2,28 @@ import pandas as pd
 import json
 import requests
 from datetime import datetime
+from math import ceil
 
-authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjhhMjE4YjJjLTdmMzgtNDU1NC1hODQ5LTVmMDlkNTg2YTM0MyIsImlhdCI6MTY3NTY0MTQ0NSwiZXhwIjoxNjkxMTkzNDQ1fQ.uTw2jCIhLXOwhhQUDFdQruYUGeyddcgP31cN8LSjEL8'
+
+def send(payload):
+    url = "http://localhost:3333/api/v1/import"
+
+    headers = {
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjhhMjE4YjJjLTdmMzgtNDU1NC1hODQ5LTVmMDlkNTg2YTM0MyIsImlhdCI6MTY3NTY0NTIyNywiZXhwIjoxNjkxMTk3MjI3fQ.Objay0z0AlfsuehulOHRPPlZY1A15Phw4kM_kYKSvwU',
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text)
+
 
 #read csv
 transactions = pd.read_csv('transReport_latest.csv')
 
 #prepare format for json
-transactions.drop(['Order ID', 'Market code', 'Exchange rate', 'Amount', 'Transaction method'], axis=1, inplace=True)
+transactions.drop(['Order ID', 'Market code', 'Exchange rate', 'Amount', 'Transaction method', 'Comments'], axis=1, inplace=True)
 transactions['dataSource'] = 'YAHOO'
-transactions['accountId'] = '52953899-2a27-4b4a-a6e2-51cca9d82387'
+transactions['accountId'] = None
 
 #rearrange columns to fit json format
 column_names = list(transactions.columns)
@@ -19,9 +31,8 @@ order = [8,6,7,0,5,2,1,4,3]
 column_names = [column_names[i] for i in order]
 transactions = transactions[column_names] 
 
-#rename columns
-transactions.columns = ['dataSource','currency', 'comments', 'date', 'fee', 'quantity', 'symbol', 'type', 'unitPrice']
-transactions['accountId'] = None
+transactions.columns = ['accountId','currency', 'dataSource','date', 'fee', 'quantity', 'symbol', 'type', 'unitPrice']
+
 
 #specify market in symbol code
 df_dict = transactions.to_dict(orient='records')
@@ -39,15 +50,31 @@ for row in df_dict:
     time_str = datetime.strptime(string, '%Y-%m-%d %H:%M:%S')
     row['date'] = time_str.isoformat()
 
+
 #convert dataframe to json object
 transactions = pd.DataFrame.from_dict(df_dict, orient="columns")
-trans_str = transactions.to_json(orient="records")
-json_str = json.dumps([{'activities':json.loads(trans_str)}])
-json_obj = json.loads(json_str)
+row_count = transactions.shape[0]
+packet_size = ceil(row_count / 10)
 
-#write json object to file and send to address
-with open('test.json', 'w') as outfile:
-    to_print = json.dumps(json_obj, indent=2)
-    outfile.write(to_print)
-    requests.post('http://192.168.1.79:3333/api/v1/import', json=to_print, headers=authToken)
+#send packets 
+start_i = 0
+for end_i in range(packet_size, row_count+1, packet_size):
+    to_send = transactions.iloc[:end_i]
+    transactions = transactions.iloc[end_i+1:]
+    
+    trans_str = to_send.to_json(orient="records")
+    json_str = json.dumps({'activities':json.loads(trans_str)})
+    json_obj = json.loads(json_str)
+    to_send = json.dumps(json_obj)    
 
+    send(to_send)
+    print(f'sent {start_i}:{end_i}')   
+    start_i = end_i+1
+
+
+# #write json object to file
+# json_obj = json.loads(json_str)
+# with open('test.json', 'w') as outfile:
+#     to_print = json.dumps(json_obj, indent=2)
+#     outfile.write(to_print)
+    
